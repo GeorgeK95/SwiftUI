e26ec4fa871afd5d1739fcdc4ba263bcbfbcc4ca
GeorgeK95 <george_it@abv.bg>
2022-11-18 16:13:54 +0200

    Initial Commit

---
+++ DemoApp/DemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoAppApp.swift
+//  DemoApp
+//
+//  Created by MG24 on 18.11.22.
+//
+
+import SwiftUI
+
+@main
+struct DemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
